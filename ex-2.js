cd8da0bcfa5578dcb19861586e7c67452558b058
GaiKT <124340777+GaiKT@users.noreply.github.com>
2023-09-08 02:15:16 +0700

    fix(ex-1, 2, 3, 4): sand assignment by gai

--- ex-2.js
+++ ex-2.js
@@ -3,16 +3,45 @@ const employees = [
   {
     name: "James",
     age: 40,
-    hobbies: ["Football", "Social media"],
+    hobbies: ["Football", "Social media"]
   },
   {
     name: "Alex",
     age: 25,
-    hobbies: ["Cryptocurrency", "Social media"],
+    hobbies: ["Cryptocurrency", "Social media"]
   },
 ];
 // Start coding here
-let alex;
-let alexHobbies;
-let alexAndJamesAge;
+let alex = employees[1].name;
+console.log(alex)
+
+employees[1].hobbies.push("Watching basketball");
+
+let alexHobbies = employees[1].hobbies;
+console.log(alexHobbies)
+
+let alexAndJamesAge = employees[0].age + employees[1].age;
+console.log(alexAndJamesAge)
+
 const updateEmployees = employees;
+
+updateEmployees.push(
+  {
+    name: "Alicia",
+    age: 29,
+    hobbies: ["Shopping", "Reading novels"]
+  },
+  {
+    name: "Kody",
+    age: 19,
+    hobbies: ["Computer games", "Wakeboard"]
+  }
+);
+
+console.log(updateEmployees);
+
+updateEmployees.pop();
+
+console.log(updateEmployees);
+
+
--- ex-4.js
+++ ex-4.js
@@ -7,7 +7,8 @@ const student = {
   subjects: ["Math", "Science", "History"],
 };
 
-console.log("Student Name: " + stdent.name);
+student.age = null;
+console.log("Student Name: " + student.name);
 console.log("Student Age: " + student.age);
-console.log("Student Grade: " + student.gade);
+console.log("Student Grade: " + student.grade);
 console.log("Student Subjects: " + student.subjects);
